b01a3b4da15e8e304626261e118cf5b217472941
Row <admin@example.com>
2026-02-10 21:35:57 +0100

    first commit

---
+++ security-spring-boot-starter/src/main/java/com/rowland/security/security_spring_boot_starter/SecuritySpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.rowland.security.security_spring_boot_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritySpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ security-spring-boot-starter/src/test/java/com/rowland/security/security_spring_boot_starter/SecuritySpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rowland.security.security_spring_boot_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
