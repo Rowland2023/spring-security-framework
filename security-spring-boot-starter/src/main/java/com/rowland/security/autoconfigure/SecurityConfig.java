d4fbbc57722d7f4daac15a2e72dbd1e7f3227502
Row <admin@example.com>
2026-02-11 16:35:27 +0100

    Add README.md file

--- security-spring-boot-starter/src/main/java/com/rowland/security/autoconfigure/SecurityConfig.java
+++ security-spring-boot-starter/src/main/java/com/rowland/security/autoconfigure/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
         http
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/v1/auth/**", "/api/public/**").permitAll()
+                .requestMatchers("/api/v1/auth/**", "/api/v1/public/**").permitAll()
                 .anyRequest().authenticated()
             )
             .sessionManagement(session -> session
